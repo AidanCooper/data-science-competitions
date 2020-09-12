e388da5fe5c6be72dfcb7eed72bbaa04b51463e1
AidanCooper <aidan@aidancooper.co.uk>
2020-09-12 18:53:06 +0100

    Create repo

---
+++ MechanismsOfAction/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Kaggle competition - can you improve the algorithm that classifies durgs based on their biological activity?',
+    author='Aidan Cooper',
+    license='MIT',
+)
